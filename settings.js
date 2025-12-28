8b3ad2f9f2cf087f370137ffbd0d067cb603e00d
ghozzz777-byte <ghozzz777@gmail.com>
2025-12-28 13:48:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,16 +2,16 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 /*
-	* Create By Naze
-	* Follow https://github.com/nazedev
-	* Whatsapp : https://whatsapp.com/channel/0029VaWOkNm7DAWtkvkJBK43
+	* Create By Azertyy
+	* Follow https://ghozzz777-byte.github.io/Azerty-store/
+	* Whatsapp : https://whatsapp.com/channel/0029VbAwpf9BlHpgOklLJj03
 */
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188'] //['628','628'] 2 owner atau lebih
-global.author = 'Nazedev'
-global.botname = 'Hitori Bot'
+global.owner = ['6283153940688'] //['628','628'] 2 owner atau lebih
+global.author = 'Azertyy'
+global.botname = 'Azerty Bot'
 global.packname = 'Bot WhatsApp'
 global.listprefix = ['+','!','.']
 
@@ -90,3 +90,4 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 });
+
