3bd3b0009e8ed20d9b51f79b95e768fe50a8f51f
ghozzz777-byte <ghozzz777@gmail.com>
2025-12-28 12:30:10 +0700

    Update naze.js

--- naze.js
+++ naze.js
@@ -2,9 +2,9 @@ process.on('uncaughtException', console.error)
 process.on('unhandledRejection', console.error)
 
 /*
-	* Create By Naze
-	* Follow https://github.com/nazedev
-	* Whatsapp : https://whatsapp.com/channel/0029VaWOkNm7DAWtkvkJBK43
+	* Create By Azertyy
+	* Follow https://ghozzz777-byte.github.io/Azerty-store/
+	* Whatsapp : https://whatsapp.com/channel/0029VbAwpf9BlHpgOklLJj03
 */
 
 require('./settings');
@@ -4588,3 +4588,4 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 });
+
